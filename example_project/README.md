dd53f44ad22a221f0a70cfe911586ceb2f0cb76a
anukul <sommer.anukul99@gmail.com>
2024-03-23 21:13:45 +0100

    init

---
+++ example_project/README.md
@@ -0,0 +1,39 @@
+# Weather App
+
+This is a simple weather application built with JavaScript. It allows users to search for weather information by location name or latitude/longitude coordinates using the OpenWeatherMap API.
+
+## Features
+
+- Add locations by entering location name or latitude/longitude coordinates.
+- Display current temperature, minimum temperature, and maximum temperature for each location.
+- Delete locations from the list.
+
+## Technologies Used
+
+- JavaScript
+- Hyperscript-helpers
+- Virtual DOM
+- OpenWeatherMap API
+
+## Installation
+
+Before running the application, you need to obtain an API key from OpenWeatherMap. Follow these steps:
+
+1. Sign up or log in to OpenWeatherMap: [OpenWeatherMap Sign Up](https://home.openweathermap.org/users/sign_up).
+2. After logging in, go to the API keys tab in your account settings.
+3. Generate a new API key if you don't have one already.
+4. Copy your API key.
+5. Go to index.js and visit the update function.
+6. Replace the "YOUR-API-KEY" with your previously created API-KEY
+
+To run this application locally, follow these steps:
+
+1. Clone the repository:
+
+2. npm install
+
+3. npm run dev
+
+## Usage
+
+You can either enter a location name (example: Basel) OR it's longitude and latitude (seperated with ",") (example: 47.559272477325166, 7.587701083636691)
--- example_project/src/index.js
+++ example_project/src/index.js
@@ -3,92 +3,105 @@ import { h, diff, patch } from "virtual-dom";
 import createElement from "virtual-dom/create-element";
 
 // allows using html tags as functions in javascript
-const { div, button, p, h1, input, table, tr, th, td } = hh(h);
+const { div, button, p, h1, input, ul, li } = hh(h);
 
 // Messages which can be used to update the model
 const MSGS = {
-  UPDATE_MEAL: "UPDATE_MEAL",
-  UPDATE_CALORIES: "UPDATE_CALORIES",
-  SAVE: "SAVE",
-  DELETE: "DELETE",
+  ADD_LOCATION: "ADD_LOCATION",
+  DELETE_LOCATION: "DELETE_LOCATION",
+  UPDATE_LOCATION: "UPDATE_LOCATION",
 };
 
 // View function which represents the UI as HTML-tag functions
 function view(dispatch, model) {
-  const { meals, calories, entries, totalCalories } = model;
+  const { locations, locationInput } = model;
 
-  const entryRows = entries.map(entry =>
-    tr({ key: entry.id, className: "border-b border-gray-200" }, [
-      td({ className: "py-2 px-4" }, entry.meals),
-      td({ className: "py-2 px-4" }, entry.calories),
-      td({ className: "py-2 px-4" }, button({ className: "text-red-500", onclick: () => dispatch({ type: MSGS.DELETE, id: entry.id }) }, "Delete"))
-    ])
-  );
+  const locationData = locations.map(location => {
+    return li({ key: location.id, className: "py-2 px-4" }, [
+      p({}, `Location: ${location.name}`),
+      p({}, `Current Temperature: ${location.currentTemp}°C`),
+      p({}, `Low Temperature: ${location.minTemp}°C`),
+      p({}, `High Temperature: ${location.maxTemp}°C`),
+      button(
+        {
+          className: "text-red-500",
+          onclick: () => dispatch({ type: MSGS.DELETE_LOCATION, id: location.id }),
+        },
+        "Delete"
+      ),
+    ]);
+  });
 
   return div({ className: "max-w-lg mx-auto mt-10" }, [
-    h1({ className: "text-3xl mb-4 text-center text-gray-800" }, `Calories Counter`),
+    h1({ className: "text-3xl mb-4 text-center text-gray-800" }, `Sommer's Weather Forecast`),
     div({ className: "flex flex-col gap-4 items-center" }, [
       input({
         type: "text",
-        placeholder: "Enter meal name",
-        value: meals,
+        placeholder: "Enter location",
+        value: locationInput,
+        oninput: (e) => dispatch({ type: MSGS.UPDATE_LOCATION, value: e.target.value }),
         className: "px-3 py-2 w-full rounded-md border border-gray-300 focus:outline-none focus:border-blue-500",
-        oninput: (e) => dispatch({ type: MSGS.UPDATE_MEAL, value: e.target.value }),
       }),
-      input({
-        type: "number",
-        placeholder: "Enter calories number",
-        value: calories,
-        className: "px-3 py-2 w-full rounded-md border border-gray-300 focus:outline-none focus:border-blue-500",
-        oninput: (e) => dispatch({ type: MSGS.UPDATE_CALORIES, value: parseInt(e.target.value) }),
-      }),
-      button({ className: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded", onclick: () => dispatch({ type: MSGS.SAVE }) }, "Save"),
+      button(
+        {
+          onclick: () => dispatch({ type: MSGS.ADD_LOCATION }),
+          className: "px-3 py-2 w-full rounded-md border border-gray-300 focus:outline-none focus:border-blue-500",
+        },
+        "Add"
+      ),
     ]),
-    div({ className: "mt-6" }, [
-      table({ className: "w-full" }, [
-        tr({ className: "bg-gray-100" }, [
-          th({ className: "py-4 px-4 text-left text-sm text-gray-600 uppercase font-semibold" }, "Meal Name"),
-          th({ className: "py-4 px-4 text-left text-sm text-gray-600 uppercase font-semibold" }, "Calories"),
-          th({ className: "py-4 px-4 text-left text-sm text-gray-600 uppercase font-semibold" }, "")
-        ]),
-        ...entryRows
-      ]),
-      p({ className: "mt-4 text-lg font-semibold" }, `Total Calories: ${totalCalories}`)
-    ])
+    div({ className: "mt-6" }, [ul({}, locationData)]),
   ]);
 }
 
 // Update function which takes a message and a model and returns a new/updated model
-function update(msg, model) {
+// Async function to fetch data from an API
+async function update(msg, model) {
   switch (msg.type) {
-    case MSGS.UPDATE_MEAL:
-      return { ...model, meals: msg.value };
+    case MSGS.ADD_LOCATION:
+      try {
+        let latitude, longitude, name;
+        const inputLoc = model.locationInput.split(',').map(part => part.trim());
+        if (inputLoc.length === 2 && !isNaN(parseFloat(inputLoc[0])) && !isNaN(parseFloat(inputLoc[1]))) {
+          latitude = parseFloat(inputLoc[0]);
+          longitude = parseFloat(inputLoc[1]);
+        } else {
+          name = model.locationInput;
+        }
 
-    case MSGS.UPDATE_CALORIES:
-      return { ...model, calories: msg.value };
+        // Use your own API-Key at the URL part: '&appid="YOUR-API-KEY"&units=metric'
+        let url;
+        if (latitude !== undefined && longitude !== undefined) {
+          url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid="YOUR-API-KEY"&units=metric`;
+        } else {
+          url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid="YOUR-API-KEY"&units=metric`;
+        }
 
-    case MSGS.SAVE:
-      const newEntry = {
-        id: Date.now(),
-        meals: model.meals,
-        calories: model.calories
-      };
-      return {
-        ...model,
-        entries: [...model.entries, newEntry],
-        totalCalories: model.totalCalories + model.calories,
-        meals: "",
-        calories: ""
-      };
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        const { main, name: cityName } = data;
+        const newLocation = {
+          id: Date.now(),
+          name: cityName,
+          currentTemp: main.temp,
+          minTemp: main.temp_min,
+          maxTemp: main.temp_max,
+        };
+        return { ...model, locations: [...model.locations, newLocation], locationInput: '' };
+      } catch (error) {
+        console.error("Error:", error);
+        return model; 
+      }
 
-    case MSGS.DELETE:
-      const updatedEntries = model.entries.filter(entry => entry.id !== msg.id);
-      const deletedEntry = model.entries.find(entry => entry.id === msg.id);
-      return {
-        ...model,
-        entries: updatedEntries,
-        totalCalories: model.totalCalories - deletedEntry.calories
-      };
+    case MSGS.DELETE_LOCATION:
+      const updatedLocations = model.locations.filter(location => location.id !== msg.id);
+      return { ...model, locations: updatedLocations };
+
+    case MSGS.UPDATE_LOCATION:
+      return { ...model, locationInput: msg.value };
 
     default:
       return model;
@@ -102,20 +115,20 @@ function app(initModel, update, view, node) {
   let rootNode = createElement(currentView);
   node.appendChild(rootNode);
   function dispatch(msg) {
-    model = update(msg, model);
-    const updatedView = view(dispatch, model);
-    const patches = diff(currentView, updatedView);
-    rootNode = patch(rootNode, patches);
-    currentView = updatedView;
+    update(msg, model).then(updatedModel => {
+      const updatedView = view(dispatch, updatedModel);
+      const patches = diff(currentView, updatedView);
+      rootNode = patch(rootNode, patches);
+      currentView = updatedView;
+      model = updatedModel;
+    });
   }
 }
 
-// The initial model when the app starts
+// Initial model at the start
 const initModel = {
-  meals: "",
-  calories: "",
-  entries: [],
-  totalCalories: 0
+  locations: [],
+  locationInput: '',
 };
 
 // The root node of the app (the div with id="app" in index.html)
